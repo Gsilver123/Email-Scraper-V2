14ac06a70e8e977f762539febee768fb95f9ebad
Garrett Silver <Gsilver@buffalo.edu>
2020-01-07 23:27:32 -0500

    Added Functional Gui
    Using PyQt, added a very basic functioning gui. Calls the GoogleScraper.py after "Go" is clicked.

--- GoogleSearcher.py
+++ GoogleSearcher.py
@@ -1,80 +1,100 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import json
 import ScrapeSite
 import time
-import CityScraper
+
+
+def start_search(location, search_list, file_location, is_email_scrape):
+    in_list = location_search(append_location_to_searches(search_list, location), is_email_scrape)
+
+    if is_email_scrape:
+        search_sites_from_list(in_list)
+    else:
+        write_file(file_location, in_list)
+
+
+def create_browser():
+    return webdriver.Chrome('C:\\Users\Garre\Downloads\chromedriver_win32\chromedriver')
+
 
 def search_sites_from_list(search_list):
-	email_list = []
-	browser = webdriver.Chrome('/Users/garrettsilver/Documents/chromedriver')
+    email_list = []
 
-	for search in search_list:
-		site_list = location_search(browser, append_cities_to_searches(search))
+    print('------------------ \nScraping emails:\n')
+    for search in search_list:
+        # Appending the cities will hbe done in the calling function, not this one
+        # site_list = location_search(append_cities_to_searches(search))
+        email_list.append(scrape_email(email_list, search))
 
-		print('------------------ \nEmails for %s:' %search)
-		for i in range(len(site_list)):
-			email = ScrapeSite.grab_email_from_site(str(site_list[i]))
-			if email and email not in email_list:
-				email_list.append(email)
-			print(str(i + 1) + ' : ' + str(len(site_list)))
-
-		file_name = ('%sEmailList.txt' %search)
-		print('Writing to ' + file_name + '\n------------------')
-
-		with open(file_name, 'w+') as email_file:
-			for i in email_list:
-				email_file.write(i + ',')
+    file_name = ('%sEmailList.txt' % search)
+    print('Writing to ' + file_name + '\n------------------')
+    write_file(file_name, email_list)
 
 
-
-def location_search(browser, search_list):
-	out_url_list = []
-
-	for i in search_list:
-		browser.get('https://www.Google.com')
-		search_bar = browser.find_element_by_name('q')
-		search_bar.send_keys(i)
-		search_bar.send_keys(Keys.ENTER)
-
-		try:
-			button = browser.find_element_by_class_name('cMjHbjVt9AZ__button')
-			button.send_keys(Keys.ENTER)
-		except Exception:
-			continue
-
-		try:
-			elems = browser.find_elements_by_class_name('yYlJEf')
-
-			print('Elems: ')
-			for x in range(5):
-				counter = 1
-				for j in elems:
-					if j.get_attribute('href').find('maps') == -1 and j.get_attribute('href').find('google') == -1 and j.get_attribute('href') not in out_url_list:
-						out_url_list.append(j.get_attribute('href'))
-						print(str(counter) + ' : ' + str(len(elems)))
-					counter += 1
-
-				browser.get(browser.find_element_by_id('pnnext').get_attribute('href'))
-				time.sleep(2)
-				elems = browser.find_elements_by_class_name('yYlJEf')
-
-		except Exception:
-			continue
-
-	browser.close()
-
-	return out_url_list
-
-def append_cities_to_searches(search):
-	city_list = CityScraper.read_and_parse_city_file()
-
-	appended_search_list = []
-	for city in city_list:
-		search_city_string = search + ' in ' + city[0] + ', ' + city[1]
-		appended_search_list.append(search_city_string)
-
-	return appended_search_list
+def scrape_email(email_list, search):
+    email = ScrapeSite.grab_email_from_site(str(search))
+    if email and email not in email_list:
+        return email
+    return None
 
 
-search_sites_from_list(['Massage Therapy', 'Physical Therapy Clinic', 'Sports Rehab Clinic', 'Rehab Center', 'Floation Therapy Spa', 'Wellness Center'])
+def write_file(file_name, list_to_write):
+    with open(file_name, 'w+') as file:
+        for i in list_to_write:
+            file.write(i + ',')
+
+
+def location_search(search_list, is_email_scrape):
+    out_list = []
+    browser = create_browser()
+
+    for i in search_list:
+        browser.get('https://www.Google.com')
+        search_bar = browser.find_element_by_name('q')
+        search_bar.send_keys(i)
+        search_bar.send_keys(Keys.ENTER)
+
+        try:
+            button = browser.find_element_by_class_name('cMjHbjVt9AZ__button')
+            button.send_keys(Keys.ENTER)
+        except Exception:
+            continue
+
+        try:
+            elems = browser.find_elements_by_class_name('yYlJEf')
+
+            print('Elems: ')
+            for x in range(5):
+                counter = 1
+                for j in elems:
+                    if j.get_attribute('href').find('maps') == -1 and j.get_attribute('href').find(
+                            'google') == -1 and j.get_attribute('href') not in out_list:
+                        out_list.append(j.get_attribute('href'))
+                        print(str(counter) + ' : ' + str(len(elems)))
+                    counter += 1
+
+                browser.get(browser.find_element_by_id('pnnext').get_attribute('href'))
+                time.sleep(2)
+                elems = browser.find_elements_by_class_name('yYlJEf')
+
+        except Exception:
+            continue
+
+    browser.close()
+
+    return out_list
+
+
+def append_location_to_searches(search_list, location):
+    # city_list = CityScraper.read_and_parse_city_file()
+
+    appended_search_list = []
+    for search in search_list:
+        search_city_string = search + ' in ' + location
+        appended_search_list.append(search_city_string)
+
+    return appended_search_list
+
+
+# if __name__ == '__main__':
+# search_sites_from_list(['Massage Therapy'])
---
+++ gui.py
@@ -0,0 +1,141 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QLineEdit, QFileDialog, QPushButton, QWidget, QMessageBox, QVBoxLayout, \
+    QDialog, QCheckBox
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import pyqtSlot
+import GoogleSearcher
+import sys
+
+
+class AppGui(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Q-Tips lil friend'
+        self.left = 500
+        self.top = 250
+        self.width = 650
+        self.height = 650
+        self.fileLocation = ''
+        self.fileName = ''
+        self.searchList = []
+        self.window()
+
+    def window(self):
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowTitle(self.title)
+
+        font = QFont("Times", 14)
+
+        self.placeTextBox(20, 70, 300, 30, "Input location you want to search:", font)
+        self.locationEditText = self.placeQLineEdit(20, 100, 250, 30, font)
+
+        self.placeTextBox(20, 170, 200, 30, "Add to search list", font)
+        self.searchListEditText = self.placeQLineEdit(20, 200, 250, 30, font)
+        self.placeButton(280, 200, 'Add', 'Add to the list with this button', font, self.onAddToSearchListClicked)
+        self.placeButton(360, 200, 'Remove', 'Remove entries with this button', font, self.onRemoveToSearchListClicked)
+
+        self.placeTextBox(20, 270, 200, 30, "Output file location:", font)
+        self.outputFileLocationEditText = self.placeQLineEdit(20, 300, 475, 30, font)
+
+        self.placeTextBox(20, 375, 200, 30, 'Enter file name:', font)
+        self.outputFileNameEditText = self.placeQLineEdit(20, 405, 250, 30, font)
+
+        self.placeButton(510, 300, 'File Search', 'Click here to find a file', font, self.onFileSearchClicked)
+
+        self.checkBox = QCheckBox(self)
+        self.checkBox.setText('Check to scape emails, else scrape phone numbers')
+        self.checkBox.setFont(font)
+        self.checkBox.setChecked(False)
+        self.checkBox.move(20, 480)
+
+        self.placeButton(20, 550, 'Meow (Go)', 'Click this. Meow', font, self.onGoClicked)
+
+        self.show()
+
+    def placeTextBox(self, xpos, ypos, width, height, text, font):
+        label = QtWidgets.QLabel(self)
+        label.setText(text)
+        label.move(xpos, ypos)
+        label.resize(width, height)
+        label.setFont(font)
+
+    def placeQLineEdit(self, xpos, ypos, width, height, font):
+        qLineEdit = QLineEdit(self)
+        qLineEdit.move(xpos, ypos)
+        qLineEdit.resize(width, height)
+        qLineEdit.setFont(font)
+
+        return qLineEdit
+
+    def placeButton(self, xpos, ypos, buttonText, toolTip, font, callback):
+        outFileSearchButton = QPushButton(buttonText, self)
+        outFileSearchButton.setToolTip(toolTip)
+        outFileSearchButton.move(xpos, ypos)
+        outFileSearchButton.setFont(font)
+        outFileSearchButton.clicked.connect(callback)
+
+    @pyqtSlot()
+    def onAddToSearchListClicked(self):
+        if self.searchListEditText.text() not in self.searchList:
+            self.searchList.append(self.searchListEditText.text())
+            self.searchListEditText.setText('')
+
+    @pyqtSlot()
+    def onRemoveToSearchListClicked(self):
+        if len(self.searchList) > 0:
+            removeSearchDialog = QVBoxLayout()
+            dialogHeight = 0
+            for i in self.searchList:
+                button = QPushButton(str(i))
+                button.clicked.connect(lambda: self.onRemoveItemFromDialog(i))
+                removeSearchDialog.addWidget(button)
+                dialogHeight += 100
+
+            self.removeDialogBox = QDialog(self)
+            self.removeDialogBox.resize(300, dialogHeight)
+            self.removeDialogBox.setWindowTitle('Click to remove')
+            self.removeDialogBox.setLayout(removeSearchDialog)
+            self.removeDialogBox.exec_()
+
+    @pyqtSlot()
+    def onRemoveItemFromDialog(self, item):
+        if item in self.searchList:
+            self.searchList.remove(item)
+            self.removeDialogBox.close()
+
+    @pyqtSlot()
+    def onFileSearchClicked(self):
+        self.openFileNameOptions()
+
+    @pyqtSlot()
+    def onGoClicked(self):
+        self.fileName = self.outputFileNameEditText.text()
+        if len(self.locationEditText.text()) <= 0 or len(self.fileName) <= 0 or len(self.searchList) <= 0 or len(self.fileLocation) <= 0:
+            errorMessage = self.createMessageBox("Meow", "Please fill all fields before continuing, Thank you", "Error")
+            errorMessage.exec_()
+        else:
+            file = self.fileLocation + '/' + self.fileName + '.csv'
+            GoogleSearcher.start_search(self.locationEditText.text(), self.searchList, file, self.checkBox.isChecked())
+            self.close()
+
+    def openFileNameOptions(self):
+        outFileDialogOptions = QFileDialog(self, "Choose File")
+        outFileDialogOptions.setFileMode(QFileDialog.DirectoryOnly)
+        outFileDialogOptions.exec_()
+        self.fileLocation = outFileDialogOptions.selectedFiles()[0]
+        self.outputFileLocationEditText.setText(self.fileLocation)
+
+    def createMessageBox(self, text, infoText, title):
+        messageBox = QMessageBox(self)
+        messageBox.setText(text)
+        messageBox.setInformativeText(infoText)
+        messageBox.setWindowTitle(title)
+
+        return messageBox
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = AppGui()
+    sys.exit(app.exec_())
