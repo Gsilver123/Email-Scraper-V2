864cda95c1142a8f06c8dd6e19df3bb16af228f0
Garrett Silver <Gsilver@buffalo.edu>
2020-01-08 13:34:27 -0500

    Scrape Phone Number Functionality
    Can now scrape phone numbers, and writes it neatly to a csv file. Also place the chrome driver inside of the main folder.

--- GoogleSearcher.py
+++ GoogleSearcher.py
@@ -8,16 +8,16 @@ def start_search(location, search_list, file_location, is_email_scrape):
     in_list = location_search(append_location_to_searches(search_list, location), is_email_scrape)
 
     if is_email_scrape:
-        search_sites_from_list(in_list)
+        search_sites_from_list(in_list, file_location)
     else:
         write_file(file_location, in_list)
 
 
 def create_browser():
-    return webdriver.Chrome('C:\\Users\Garre\Downloads\chromedriver_win32\chromedriver')
+    return webdriver.Chrome('chromedriver_win32\chromedriver')
 
 
-def search_sites_from_list(search_list):
+def search_sites_from_list(search_list, file_location):
     email_list = []
 
     print('------------------ \nScraping emails:\n')
@@ -26,9 +26,8 @@ def search_sites_from_list(search_list):
         # site_list = location_search(append_cities_to_searches(search))
         email_list.append(scrape_email(email_list, search))
 
-    file_name = ('%sEmailList.txt' % search)
-    print('Writing to ' + file_name + '\n------------------')
-    write_file(file_name, email_list)
+    print('Writing to ' + file_location + '\n------------------')
+    write_file(file_location, email_list)
 
 
 def scrape_email(email_list, search):
@@ -41,7 +40,7 @@ def scrape_email(email_list, search):
 def write_file(file_name, list_to_write):
     with open(file_name, 'w+') as file:
         for i in list_to_write:
-            file.write(i + ',')
+            file.write(i + '\n')
 
 
 def location_search(search_list, is_email_scrape):
@@ -57,28 +56,58 @@ def location_search(search_list, is_email_scrape):
         try:
             button = browser.find_element_by_class_name('cMjHbjVt9AZ__button')
             button.send_keys(Keys.ENTER)
-        except Exception:
+        except Exception as ex:
+            print(str(ex))
             continue
 
-        try:
-            elems = browser.find_elements_by_class_name('yYlJEf')
-
-            print('Elems: ')
-            for x in range(5):
-                counter = 1
-                for j in elems:
-                    if j.get_attribute('href').find('maps') == -1 and j.get_attribute('href').find(
-                            'google') == -1 and j.get_attribute('href') not in out_list:
-                        out_list.append(j.get_attribute('href'))
-                        print(str(counter) + ' : ' + str(len(elems)))
-                    counter += 1
-
-                browser.get(browser.find_element_by_id('pnnext').get_attribute('href'))
-                time.sleep(2)
+        if is_email_scrape:
+            try:
                 elems = browser.find_elements_by_class_name('yYlJEf')
 
-        except Exception:
-            continue
+                print('Elems: ')
+                for x in range(5):
+                    counter = 1
+                    for j in elems:
+                        if j.get_attribute('href').find('maps') == -1 and j.get_attribute('href').find(
+                                'google') == -1 and j.get_attribute('href') not in out_list:
+                            out_list.append(j.get_attribute('href'))
+                            print(str(counter) + ' : ' + str(len(elems)))
+                        counter += 1
+
+                    browser.get(browser.find_element_by_id('pnnext').get_attribute('href'))
+                    time.sleep(2)
+                    elems = browser.find_elements_by_class_name('yYlJEf')
+
+            except Exception as e:
+                print(str(e))
+                continue
+        else:
+            try:
+                elems = browser.find_elements_by_class_name('cXedhc')
+
+                print('Elems: ')
+                for x in range(5):
+                    counter = 1
+                    for j in elems:
+                        try:
+                            title = str(j.find_element_by_xpath('div/div').text)
+                            title.replace('\n', ' ')
+                            number = str(j.find_element_by_xpath('span/div[3]/span[2]').text)
+                            out_list.append(title + ", " + number)
+
+                        except Exception as exc:
+                            print('Could not retrieve name or phone number: error: ' + str(exc))
+
+                        print(str(counter) + ' : ' + str(len(elems)))
+                        counter += 1
+
+                    browser.get(browser.find_element_by_id('pnnext').get_attribute('href'))
+                    time.sleep(2)
+                    elems = browser.find_elements_by_class_name('cXedhc')
+
+            except Exception as e:
+                print(str(e))
+                continue
 
     browser.close()
 
